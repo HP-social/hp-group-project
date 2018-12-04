39dcfbbd663d643fc30c04887d3c0dfccbc9e7bc
Patric Khiev <webdev.patric@gmail.com>
2018-12-04 13:38:25 -0600

    landing partially built

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,25 +1,37 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { setUser } from '../../ducks/reducer';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { setUser } from "../../ducks/reducer";
+import "./LandingPage.scss";
 
 class LandingPage extends Component {
-	constructor() {
-		super();
-		this.state = {};
-	}
-	render() {
-		return <div>LandingPage</div>;
-	}
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="main_landing">
+        <div className="img_overlay" />
+        <div className="top_castle" />
+        <div className="mid_container">
+					<div className='card'>1</div>
+					<div className='card'>2</div>
+					<div className='card'>3</div>
+				</div>
+        <div className="bottom_castle">hi3</div>
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-	const { user } = state;
-	return {
-		user
-	};
+  const { user } = state;
+  return {
+    user
+  };
 }
 
 export default connect(
-	mapStateToProps,
-	{ setUser }
+  mapStateToProps,
+  { setUser }
 )(LandingPage);
