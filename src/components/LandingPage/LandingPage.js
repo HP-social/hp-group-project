1eb862bacfe8e79a9e381cd136cfad69cc5190c9
Patric Khiev <webdev.patric@gmail.com>
2018-12-04 17:09:17 -0600

    added sections to landing page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -9,16 +9,37 @@ class LandingPage extends Component {
     this.state = {};
   }
   render() {
+    const icon = [
+      "https://image.flaticon.com/icons/svg/859/859170.svg",
+      "https://image.flaticon.com/icons/svg/784/784629.svg",
+      "https://image.flaticon.com/icons/svg/149/149054.svg"
+    ];
+    const text = [
+      "Join A Noble House",
+      "Interact In Public & Private Communites",
+      "Discover Adventure Throughout Hogwarts!"
+    ];
+    const cardTitle = ["Houses", "Communities", "Explore"].map((e, i) => {
+      return (
+        <div className="card" key={i}>
+          <>
+            <img src={icon[i]} alt="oops" />
+          </>
+          <>
+            <h1>{e}</h1>
+          </>
+          <>
+            <p>{text[i]}</p>
+          </>
+        </div>
+      );
+    });
     return (
       <div className="main_landing">
         <div className="img_overlay" />
         <div className="top_castle" />
-        <div className="mid_container">
-					<div className='card'>1</div>
-					<div className='card'>2</div>
-					<div className='card'>3</div>
-				</div>
-        <div className="bottom_castle">hi3</div>
+        <div className="mid_container">{cardTitle}</div>
+        <div className="bottom_container">hi</div>
       </div>
     );
   }
