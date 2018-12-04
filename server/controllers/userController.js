758c2e5ce6d660acbbc5d5bb408608ff7c080bd1
Emmanuel Bravo <eebravo1@gmail.com>
2018-12-04 14:40:41 -0600

    added newsfeed endpoint

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -7,15 +7,20 @@ module.exports = {
 				res.status(200).json(result);
 			})
 			.catch((err) => res.status(500).send(err));
+	},
+	getNews: (req, res, next) => {
+		req.app
+			.get('db')
+			.query(
+				`select like_table.likes,forum_post.title,forum_post.post_id,forum_post.forum_id,forum_post.wizard_id,forum_post.post,forum_post.time,wizards.username,wizards.house,wizards.role,wizards.profile_img from forum_post join wizards on wizards.wizard_id=forum_post.wizard_id join subscribe on subscribe.forum_id=forum_post.forum_id join follow on follow.followed_id=forum_post.wizard_id join (select count(*)as likes, post_id from likes group by post_id) as like_table on like_table.post_id=forum_post.post_id where follow.follower_id=${
+					req.params.id
+				} or subscribe.wizard_id=${
+					req.params.id
+				} order by forum_post.time, like_table.likes desc`
+			)
+			.then((result) => {
+				res.status(200).json(result);
+			})
+			.catch((err) => res.status(500).send(err));
 	}
-	// ,
-	// getHome: (req, res, next) => {
-	// 	req.app
-	// 		.get('db')
-	// 		.query(`select * from wizards where wizard_id=${req.params.id}`)
-	// 		.then((result) => {
-	// 			res.status(200).json(result);
-	// 		})
-	// 		.catch((err) => res.status(500).send(err));
-	// }
 };
