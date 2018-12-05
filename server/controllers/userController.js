343a93b94128683944f99d4fa95c028e97bf6364
Emmanuel Bravo <eebravo1@gmail.com>
2018-12-05 09:13:58 -0600

    added proxy and edited user endpoint

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -12,7 +12,7 @@ module.exports = {
 		req.app
 			.get('db')
 			.query(
-				`select like_table.likes,forum_post.title,forum_post.post_id,forum_post.forum_id,forum_post.wizard_id,forum_post.post,forum_post.time,wizards.username,wizards.house,wizards.role,wizards.profile_img from forum_post join wizards on wizards.wizard_id=forum_post.wizard_id join subscribe on subscribe.forum_id=forum_post.forum_id join follow on follow.followed_id=forum_post.wizard_id join (select count(*)as likes, post_id from likes group by post_id) as like_table on like_table.post_id=forum_post.post_id where follow.follower_id=${
+				`select distinct like_table.likes,forum_post.gif,forum_post.title,forum.location,forum_post.post_id,forum_post.forum_id,forum_post.wizard_id,forum_post.post,forum_post.time,wizards.username,wizards.house,wizards.role,wizards.profile_img from forum_post join wizards on wizards.wizard_id=forum_post.wizard_id join subscribe on subscribe.forum_id=forum_post.forum_id join forum on forum.forum_id=forum_post.forum_id join follow on follow.followed_id=forum_post.wizard_id join (select count(*)as likes, post_id from likes group by post_id) as like_table on like_table.post_id=forum_post.post_id where follow.follower_id=${
 					req.params.id
 				} or subscribe.wizard_id=${
 					req.params.id
