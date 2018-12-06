c0d6395d72d97b9f18145660a26f140117a5166f
Aaron Estes <aestesc@gmail.com>
2018-12-06 10:41:43 -0600

    changed the styling of the quiz

--- src/components/Login/SortingHat/SortingHat.js
+++ src/components/Login/SortingHat/SortingHat.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { setUser } from "../../../ducks/reducer";
-import "./SortingHat.scss";
-import axios from "axios";
-import swal from "sweetalert2";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { setUser } from '../../../ducks/reducer';
+import './SortingHat.scss';
+import axios from 'axios';
+import swal from 'sweetalert2';
 
 class SortingHat extends Component {
   constructor() {
@@ -16,7 +16,12 @@ class SortingHat extends Component {
       gryffindor: 0,
       ravenclaw: 0,
       hufflepuff: 0,
-      questions: [],
+      questions: [
+        {
+          question_id: 1,
+          text: 'If you could have one superpower what would it be'
+        }
+      ],
       answers: [],
       winning_house: [],
       crest: [],
@@ -26,11 +31,11 @@ class SortingHat extends Component {
 
   componentDidMount() {
     axios
-      .get("http://localhost:3001/api/quiz/questions")
+      .get('http://localhost:3001/api/quiz/questions')
       .then(result => this.setState({ questions: result.data }));
 
     axios
-      .get("http://localhost:3001/api/quiz/answers")
+      .get('http://localhost:3001/api/quiz/answers')
       .then(result => this.setState({ answers: result.data }));
   }
   change() {
@@ -51,10 +56,10 @@ class SortingHat extends Component {
       () => this.change()
     );
     // console.log(item)
-    console.log("gryffindor", this.state.gryffindor);
-    console.log("ravenclaw", this.state.ravenclaw);
-    console.log("hufflepuff", this.state.hufflepuff);
-    console.log("slytherin", this.state.slytherin);
+    console.log('gryffindor', this.state.gryffindor);
+    console.log('ravenclaw', this.state.ravenclaw);
+    console.log('hufflepuff', this.state.hufflepuff);
+    console.log('slytherin', this.state.slytherin);
   }
 
   returnResultsPage() {
@@ -66,9 +71,9 @@ class SortingHat extends Component {
     ) {
       this.setState(
         {
-          winning_house: "Ravenclaw",
+          winning_house: 'Ravenclaw',
           crest:
-            "https://images-na.ssl-images-amazon.com/images/I/61XvQSdFHlL._SY450_.jpg",
+            'https://images-na.ssl-images-amazon.com/images/I/61XvQSdFHlL._SY450_.jpg',
           gif: 'https://media.giphy.com/media/1UAIoVWRhtjhe/giphy.gif'
         },
         () => name()
@@ -80,8 +85,9 @@ class SortingHat extends Component {
     ) {
       this.setState(
         {
-          winning_house: "Slytherin",
-          crest: "https://www.logolynx.com/images/logolynx/37/37c001eedb4d6ff394a8635f7e8b978d.png",
+          winning_house: 'Slytherin',
+          crest:
+            'https://www.logolynx.com/images/logolynx/37/37c001eedb4d6ff394a8635f7e8b978d.png',
           gif: 'https://media.giphy.com/media/Gyb7Mrx7jlGF2/giphy.gif'
         },
         () => name()
@@ -93,32 +99,34 @@ class SortingHat extends Component {
     ) {
       this.setState(
         {
-          winning_house: "Gryffindor",
-          crest: "https://i.pinimg.com/originals/1a/64/49/1a6449ba53c49b816d56c5abbbb1d8dc.jpg",
+          winning_house: 'Gryffindor',
+          crest:
+            'https://i.pinimg.com/originals/1a/64/49/1a6449ba53c49b816d56c5abbbb1d8dc.jpg',
           gif: 'https://media.giphy.com/media/Tl2AK8HOHj7SU/giphy.gif'
-
         },
         () => name()
       );
     } else {
       this.setState(
         {
-          winning_house: "Hufflepuff",
-          crest: "http://jkfloodrelief.org/wp-content/uploads/2018/05/img-beautiful-hufflepuff-crest-pottermore.png",
+          winning_house: 'Hufflepuff',
+          crest:
+            'http://jkfloodrelief.org/wp-content/uploads/2018/05/img-beautiful-hufflepuff-crest-pottermore.png',
           gif: 'https://media.giphy.com/media/1UAIoVWRhtjhe/giphy.gif'
         },
         () => name()
       );
     }
 
-
     let name = () => {
       swal({
-        title: "Congratulations!",
+        title: 'Congratulations!',
         text: `You are a ${this.state.winning_house}`,
         imageUrl: `${this.state.crest}`,
         imageWidth: 150,
         imageHeight: 150,
+
+        background: '#fff url()',
         backdrop: `url(${this.state.gif})
     		center left
 			no-repeat`
@@ -140,21 +148,27 @@ class SortingHat extends Component {
             {item.answer}
           </div>
         ));
-    console.log("the winning house is", this.state.winning_house);
+    console.log('the winning house is', this.state.winning_house);
     return (
-      <div className='sortingHatQuestionBox'>
-        <div className='quiz'>Harry Potter</div>
+      <div className='theQuiz'>
         <img
-          className='sortingHatPicture'
-          src='https://www.hp-lexicon.org/wp-content/uploads/2016/09/the_sorting_hat_by_sahinduezguen-d47mwt5.png'
-          alt='text'
+          className='greatHallBackground'
+          src='https://vignette.wikia.nocookie.net/harrypotter/images/b/bd/High_Table_PS.png/revision/latest?cb=20161120225850'
         />
-        <div className='sortingHatQuestion'>
-          {this.state.questions[5] &&
-            this.state.questions[this.state.index].text}
-        </div>
-        <div className='sortingHatAnswers'>
-          {this.state.index < 18 && answers}
+        <div className='sortingHatQuestionBox'>
+          <div id='quiz_name'>Sorting Hat Quiz</div>
+          <img
+            className='sortingHatPicture'
+            src='https://www.hp-lexicon.org/wp-content/uploads/2016/09/the_sorting_hat_by_sahinduezguen-d47mwt5.png'
+            alt='text'
+          />
+          <div className='sortingHatQuestion'>
+            {this.state.questions[5] &&
+              this.state.questions[this.state.index].text}
+          </div>
+          <div className='sortingHatAnswers'>
+            {this.state.index < 18 && answers}
+          </div>
         </div>
       </div>
     );
