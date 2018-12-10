675a59a174a1d78216a4ff120876708d301ab33c
Aaron Estes <aestesc@gmail.com>
2018-12-10 08:56:30 -0600

    put a post in a forum

--- src/components/Forum/Forum.js
+++ src/components/Forum/Forum.js
@@ -10,21 +10,19 @@ class Forum extends Component {
     super(props);
     this.state = {
       post: [],
-	  makeATweet: false,
-	  user: { email: 'eebravo1@gmail.com', wizard_id: 1 }
-
+      makeATweet: false,
+      user: { email: 'aestesc@gmail.com', wizard_id: 3, username: 'ronstoppable', house: 'slytherin', profile_img: 'https://d36tnp772eyphs.cloudfront.net/blogs/1/2016/01/BENH9926-Edit.jpg' }
     };
   }
 
-  componentDidUpdate () {
-	  console.log('hi')
+  componentDidUpdate() {
+    console.log('hi');
     axios.get(`/api/forum/posts/${this.match.params.id}`).then(results => {
       this.setState({
         post: results.data
       });
       console.log('params');
-	});
-	
+    });
   }
 
   tweet = () => {
@@ -32,11 +30,23 @@ class Forum extends Component {
   };
 
   render() {
-	  console.log(this.props)
-	  
+    console.log(this.props);
+
     return (
-      <div className='poop'>Teeeeeeext
-	  <div className='forum_post'></div>
+      <div className='everything'>
+        <h1 className='forum_title'>Slytherin Commons</h1>
+        <div className='forum_post'>
+          Ron is the best wizard
+          <img id='wizard_avi'src={this.state.user.profile_img}></img>
+          <div id='wizard_name'>{this.state.user.username}</div>
+          <div id='time_of_post'>2 hours ago</div>
+          <div id='star-five' />
+          <div id='star-five_number'>39</div>
+          <div id='comment' />
+          <div id ='comment_number'>8</div>
+          <div id='triangle' />
+        </div>
+
         <button className='tweetButton' onClick={() => this.tweet()}>
           <img src='https://image.flaticon.com/icons/svg/1305/1305386.svg' />
         </button>
