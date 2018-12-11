0ee4899ef8f469e402ba78ca8583c4f3e05e5c72
Patric Khiev <webdev.patric@gmail.com>
2018-12-11 17:17:04 -0600

    profile follwers dynamic

--- src/ducks/reducer.js
+++ src/ducks/reducer.js
@@ -2,7 +2,6 @@ const axios = require('axios');
 const SET_USER = 'SET_USER';
 const PPL_YOU_FOLLOW = 'PPL_YOU_FOLLOW';
 
-
 const initialState = {
   user: {},
   forumPosts: [],
@@ -12,10 +11,8 @@ const initialState = {
   messagesCount: [],
   mentionsCount: [],
   //
-  youFollow: [],
-
-
-
+  youFollow: {count:0},
+  followingYou: {count:0},
 };
 
 function reducer(state = initialState, action) {
@@ -41,8 +38,8 @@ export function setUser() {
 export function peopleYouFollow(id) {
   return {
     type: PPL_YOU_FOLLOW,
-    payload: axios.get(`/api/followed/${id}`).then(response => {
-      return response.data;
+    payload: axios.get(`/api/follwernumber/${id}`).then(response => {
+      return response.data[0];
     })
   };
 }
