dfe667a17ceed2412b6d61d6d64ab0c3f8229a33
Patric Khiev <webdev.patric@gmail.com>
2018-12-12 14:05:37 -0600

    pending sub component

--- src/components/Favorites/Subscriptions/Subscriptions.js
+++ src/components/Favorites/Subscriptions/Subscriptions.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import './Subscriptions.scss';
+import HouseHeader from '../../Tools/HouseHeader/HouseHeader';
 
 class Subscriptions extends Component {
-	constructor() {
-		super();
-		this.state = {};
-	}
-	render() {
-		return <div>Subscriptions</div>;
-	}
+  constructor() {
+    super();
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className='subs_main'>
+        {/* <HouseHeader>Subscriptions</HouseHeader> */}
+				<h1>hello</h1>
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
-	const { user } = state;
-	return {
-		user
-	};
+  const { user } = state;
+  return {
+    user
+  };
 }
 
 export default connect(mapStateToProps)(Subscriptions);
