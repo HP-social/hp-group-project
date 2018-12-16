38f0a0b0c22ca8cfc90ed5934dd684cc36757a9a
Patric Khiev <webdev.patric@gmail.com>
2018-12-16 16:44:57 -0600

    5 more test

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Enzyme, { shallow } from 'enzyme';
+import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { checkPropTypes, string } from 'prop-types';
+import { checkPropTypes, string, array } from 'prop-types';
 import configureStore from 'redux-mock-store';
 
-//**** COMPONENTS TO BE TESTED **** 
+//**** COMPONENTS TO BE TESTED ****
 import LandingPage from './components/LandingPage/LandingPage';
 import HouseHeader from './components/Tools/HouseHeader/HouseHeader';
 import Navigation from './components/Navigation/Navigation';
 import BottomNav from './components/Navigation/BottomNav';
+import Profile from './components/Profile/Profile';
+import Subscriptions from './components/Favorites/Subscriptions/Subscriptions';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-const initialState = { user: { house: 'gryffindor', username: 'patronuskiller'} };
+const initialState = {
+  user: { house: 'gryffindor', username: 'patronuskiller', wizard_id: 2 },
+  followingYou: { count: 0 }
+};
 const mockStore = configureStore();
 // let wrapper;
 // let store;
@@ -53,8 +58,39 @@ test('house header props should be ravenclaw', () => {
   const wrapper = shallow(<HouseHeader store={store} house='ravenclaw' />);
   expect(wrapper.props().house).toEqual('ravenclaw');
 });
+
+test('Navigation component renders after user login', () => {
+  const store = mockStore(initialState);
+  const wrapper = shallow(<Navigation store={store} />);
+  expect(wrapper.exists()).toBe(true);
+});
 test('username should be a dynamic string', () => {
   const store = mockStore(initialState);
-  const wrapper = shallow(<Navigation store={store} username='patronuskiller'/>);
+  const wrapper = shallow(
+    <Navigation store={store} username='patronuskiller' />
+  );
   expect(wrapper.props().username).toEqual('patronuskiller');
 });
+test('Profile Comonent renders', () => {
+  const store = mockStore(initialState);
+  const wrapper = shallow(<Profile store={store} />);
+  expect(wrapper.exists()).toBe(true);
+});
+test('Profile Comonent wizard ID should be 2', () => {
+  const store = mockStore(initialState);
+  const wrapper = shallow(<Profile store={store} />);
+  expect(wrapper.props().user.wizard_id).toBe(2);
+});
+
+test('Profile Comonent followYou count should be 0', () => {
+  const store = mockStore(initialState);
+  const wrapper = shallow(<Profile store={store} />);
+  expect(wrapper.props().followingYou.count).toBe(0);
+});
+
+//MOUNT 
+// test('profile component wizard ID should be 2', () => {
+//   const store = mockStore(initialState);
+//   const wrapper = mount(<Profile store={store} />);
+//   expect(wrapper.props().followingYou.count).toBe(0);
+// });
